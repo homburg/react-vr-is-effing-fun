7fccd5880140d1ab5ffbf39c24257b60e91b3adc
Thomas B Homburg <thomas@homburg.dk>
2017-08-19 11:18:14 +0200

    Lost in translations

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,88 @@
 import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import {AppRegistry, asset, Pano, Text, View} from 'react-vr';
+import throttle from 'lodash/fp/throttle';
+
+function dump(x) {
+  return JSON.stringify(x, null, 2);
+}
+
+function rotator(from, by, to) {
+  return function(value) {
+    if (arguments.length === 0) {
+      return from;
+    }
+
+    const newValue = value + by;
+    if (newValue > to) {
+      return from;
+    }
+    return newValue;
+  };
+}
+
+const r = rotator(-3, 0.005, 3);
+
+const textStyle = {
+  fontSize: 0.8,
+  color: 'black',
+  fontWeight: '400',
+  textAlign: 'left',
+  textAlignVertical: 'center',
+};
 
 export default class hello_react_vr extends React.Component {
+  state = {
+    x: r(),
+  };
+
+  componentDidMount() {
+    console.log('Did mount');
+    setInterval(this.animate, 1000 / 60);
+  }
+
+  animate = x => {
+    this.setState(oldState => ({...oldState, x: r(oldState.x)}));
+  };
+
+  handleTextLayout = ({nativeEvent: {width}}) => {
+    this.updateWidth(width);
+  };
+
+  updateWidth = throttle(200, width => {
+    this.setState(({width: oldWidth, ...oldState}) => {
+      oldWidth = oldWidth || 0;
+      return {...oldState, width: Math.max(oldWidth, width)};
+    });
+  });
+
   render() {
+    const {x, width} = this.state;
+
+    const widthStyle = width ? {width} : {};
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+        <Pano source={asset('chess-world.jpg')} />
+        <View
+          onLayout={this.handleTextLayout}
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
+            backgroundColor: 'lightgray',
             layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            padding: 0.3,
+            transform: [
+              {translate: [x, 0, -6]},
+              {rotate: x * 60},
+              {rotateX: -1 * x * 600},
+              {rotateY: -1 * x * 300},
+            ],
+            ...widthStyle,
           }}>
-          hello
-        </Text>
+          <Text style={textStyle}>
+            Hello {x.toFixed(3)} {width && width.toFixed(3)}
+          </Text>
+        </View>
       </View>
     );
   }
-};
+}
 
 AppRegistry.registerComponent('hello_react_vr', () => hello_react_vr);
