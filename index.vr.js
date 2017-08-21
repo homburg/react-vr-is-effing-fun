629aa47149b20c7da541e2d407dfa81eab1f85e4
Thomas B Homburg <thomas@homburg.dk>
2017-08-21 21:13:26 +0200

    Planes and colors

--- index.vr.js
+++ index.vr.js
@@ -1,6 +1,38 @@
 import React from 'react';
-import {AppRegistry, asset, Pano, Text, View} from 'react-vr';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  Box,
+  DirectionalLight,
+  Plane,
+} from 'react-vr';
 import throttle from 'lodash/fp/throttle';
+import sample from 'lodash/fp/sample';
+
+const color = {
+  red: 'rgb(244, 67, 54)',
+  pink: 'rgb(233, 30, 99)',
+  purple: 'rgb(156, 39, 176)',
+  deepPurple: 'rgb(103, 58, 183)',
+  indigo: 'rgb(63, 81, 181)',
+  blue: 'rgb(33, 150, 243)',
+  lightBlue: 'rgb(3, 169, 244)',
+  cyan: 'rgb(0, 188, 212)',
+  teal: 'rgb(0, 150, 136)',
+  green: 'rgb(76, 175, 80)',
+  lightGreen: 'rgb(139, 195, 74)',
+  lime: 'rgb(205, 220, 57)',
+  yellow: 'rgb(255, 235, 59)',
+  amber: 'rgb(255, 193, 7)',
+  orange: 'rgb(255, 152, 0)',
+  deepOrange: 'rgb(255, 87, 34)',
+  brown: 'rgb(121, 85, 72)',
+  grey: 'rgb(158, 158, 158)',
+  blueGrey: 'rgb(96, 125, 139)',
+};
 
 function dump(x) {
   return JSON.stringify(x, null, 2);
@@ -20,6 +52,23 @@ function rotator(from, by, to) {
   };
 }
 
+const dim = 0.4;
+const margin = 0.3;
+
+function B({style, ...props}) {
+  const styl = {margin, ...style};
+  return (
+    <Box
+      {...props}
+      style={styl}
+      dimDepth={dim}
+      dimWidth={dim}
+      dimHeight={dim}
+      lit={true}
+    />
+  );
+}
+
 const r = rotator(-3, 0.005, 3);
 
 const textStyle = {
@@ -32,12 +81,19 @@ const textStyle = {
 
 export default class hello_react_vr extends React.Component {
   state = {
-    x: r(),
+    // x: r(),
+    x: -2,
+    dim,
   };
 
   componentDidMount() {
     console.log('Did mount');
-    setInterval(this.animate, 1000 / 60);
+    // setInterval(this.animate, 1000);
+  }
+
+  componentDidReceiveProps() {
+    console.log('Now?');
+    return;
   }
 
   animate = x => {
@@ -55,30 +111,56 @@ export default class hello_react_vr extends React.Component {
     });
   });
 
+  getTransform(x) {
+    return [
+      {translate: [-1, 1, -3]},
+      ...['rotateX', 'rotateY', 'rotateZ'].map(key => ({[key]: 45})),
+    ];
+    [
+      {translate: [x, 0, -6]},
+      {rotate: x * 60},
+      {rotateX: -1 * x * 600},
+      {rotateY: -1 * x * 300},
+    ];
+  }
+
   render() {
-    const {x, width} = this.state;
+    const {x, width, dim} = this.state;
 
     const widthStyle = width ? {width} : {};
     return (
       <View>
         <Pano source={asset('chess-world.jpg')} />
+        <DirectionalLight />
         <View
           onLayout={this.handleTextLayout}
           style={{
-            backgroundColor: 'lightgray',
-            layoutOrigin: [0.5, 0.5],
+            flex: 1,
+            flexDirection: 'column',
+            alignItems: 'stretch',
             padding: 0.3,
-            transform: [
-              {translate: [x, 0, -6]},
-              {rotate: x * 60},
-              {rotateX: -1 * x * 600},
-              {rotateY: -1 * x * 300},
-            ],
-            ...widthStyle,
+            transform: this.getTransform(x),
           }}>
-          <Text style={textStyle}>
-            Hello {x.toFixed(3)} {width && width.toFixed(3)}
-          </Text>
+          <B />
+          <Plane
+            style={{
+              transform: [{rotateX: 90}],
+              height: dim + margin,
+            }}
+            wireframe={true}>
+            {Array.from({length: 2}).map((_, i) =>
+              <View key={i} style={{flex: 1, flexDirection: 'row'}}>
+                <B />
+                <B />
+              </View>,
+            )}
+          </Plane>
+          <B />
+          {/*
+          <B style={{color: color.red}} />
+			 <B
+            style={{color: sample(color), transform: [{translate: [0, 0, -1]}]}}
+          />  */}
         </View>
       </View>
     );
